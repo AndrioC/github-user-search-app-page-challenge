0f69ad7fe6199e2765e06597918b933659f7a2a1
Andrio <andrio.rodrigo.silva@hotmail.com>
2021-09-08 08:04:08 -0300

    project started

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root {
+        --white: #FFFFFF;
+
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-color: var(--background);
+        height: 100%;
+    }
+
+    html {
+        position: relative;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Alata', sans-serif;
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-family: 'Alata', sans-serif;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    ::-webkit-scrollbar {
+        width: 0px;
+    }
+
+    @media (max-width: 960px) {
+    body {
+       overflow-x: hidden;
+    }
+}
+`;
